24ff92cbe0c1264dd86876690e15bf6f61147a8d
DogeSSBM <dogessbm@gmail.com>
2022-06-08 17:10:20 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -14,6 +14,22 @@ int main()
     while(true){
         Ticks t = frameStart();
 
+        if(keyPressed(SDL_SCANCODE_UP)){
+            facing = DIR_U;
+        }
+
+        if(keyPressed(SDL_SCANCODE_RIGHT)){
+            facing = DIR_R;
+        }
+
+        if(keyPressed(SDL_SCANCODE_DOWN)){
+            facing = DIR_D;
+        }
+
+        if(keyPressed(SDL_SCANCODE_LEFT)){
+            facing = DIR_L;
+        }
+
         pos = coordShift(pos, facing, 1);
         fillSquareCoord(pos, scale);
 
