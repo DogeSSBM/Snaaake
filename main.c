7ee74cc2e1aa145d5aba3c28e7fb7d291167e33a
DogeSSBM <dogessbm@gmail.com>
2022-06-08 16:32:47 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -2,18 +2,19 @@
 
 int main()
 {
+    const uint scale = 64;
     Length window = {800, 600};
     init();
     setWindowLen(window);
 
     setColor(PINK);
-    Coord pos = coordOffset(getWindowMid(), iC(-64,-64));
+    Coord pos = coordOffset(getWindowMid(), iC(-scale/2,-scale/2));
 
     while(true){
         Ticks t = frameStart();
 
         pos.x = pos.x+1;
-        fillSquareCoord(pos, 128);
+        fillSquareCoord(pos, scale);
 
         frameEnd(t);
     }
